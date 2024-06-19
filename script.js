97c11a7749131d1458c4cdd26aa0714abbaf50b9
Uttio Mukherjee <mukherjeeuttio@gmail.com>
2024-06-19 21:06:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let countHome = document.getElementById("home-disp");
+
+function Home(count) {
+    countHome.textContent = parseInt(countHome.textContent) + count;
+}
+
+let countGuest = document.getElementById("guest-disp");
+
+function Guest(count) {
+    countGuest.textContent = parseInt(countGuest.textContent) + count;
+}
+
